6489a5e312fdcc035b5be1ef54e8308489af41dc
Santiago Colombatto <sjcolombatto@hotmail.com>
2020-07-16 11:28:48 -0300

    AI Started

---
+++ Assets/Script/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{   
+    private Transform target;
+    [SerializeField]
+    private float speed = 7f;
+    [SerializeField]
+    private float range;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = FindObjectOfType<PlayerController>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPlayer();
+    }
+
+    public void FollowPlayer(){
+        transform.position = Vector3.MoveTowards(transform.position,target.transform.position,speed * Time.deltaTime);
+    }
+}
